6c33e5772db2ea3fb7e4a5cf8f12fd122671ac6d
Filipe Tavares <filipetrade@outlook.com>
2018-06-21 15:04:14 -0300

    Projeto criado

---
+++ src/main/java/com/tradenorte/aplicacaoTradeNorte/AplicacaoTradeNorteApplication.java
@@ -0,0 +1,12 @@
+package com.tradenorte.aplicacaoTradeNorte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplicacaoTradeNorteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplicacaoTradeNorteApplication.class, args);
+	}
+}
